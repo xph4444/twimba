5c5e63f9b04f5da7f08f424d0ca288f27d413855
philxu <xph4444@gmail.com>
2024-08-06 19:51:48 +0800

    Update data.js
    add another ''/'' at the beginning of the image url

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
-export const tweetsData = [   
+export const tweetsData = [
     {
         handle: `@TrollBot66756542 💎`,
-        profilePic: `images/troll.jpg`,
+        profilePic: `/images/troll.jpg`,
         likes: 27,
         retweets: 10,
         tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
@@ -10,22 +10,22 @@ export const tweetsData = [
         isLiked: false,
         isRetweeted: false,
         uuid: '4b161eee-c0f5-4545-9c4b-8562944223ee',
-    },    
+    },
     {
         handle: `@Elon ✅`,
-        profilePic: `images/musk.png`,
+        profilePic: `/images/musk.png`,
         likes: 6500,
         retweets: 234,
         tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
         replies: [
-                  {
+            {
                 handle: `@TomCruise ✅`,
-                profilePic: `images/tcruise.png`,
+                profilePic: `/images/tcruise.png`,
                 tweetText: `Yes! Sign me up! 😎🛩`,
             },
-                  {
+            {
                 handle: `@ChuckNorris ✅`,
-                profilePic: `images/chucknorris.jpeg`,
+                profilePic: `/images/chucknorris.jpeg`,
                 tweetText: `I went last year😴`,
             },
         ],
@@ -33,9 +33,9 @@ export const tweetsData = [
         isRetweeted: false,
         uuid: '3c23454ee-c0f5-9g9g-9c4b-77835tgs2',
     },
-        {
+    {
         handle: `@NoobCoder12`,
-        profilePic: `images/flower.png`,
+        profilePic: `/images/flower.png`,
         likes: 10,
         retweets: 3,
         tweetText: `Are you a coder if you only know HTML?`,
@@ -54,5 +54,5 @@ export const tweetsData = [
         isLiked: false,
         isRetweeted: false,
         uuid: '8hy671sff-c0f5-4545-9c4b-1237gyys45',
-    },     
+    },
 ]
