3d4419d5368290638de08033365d777501a0ff14
philxu <xph4444@gmail.com>
2024-08-07 14:01:02 +0800

    Update data.js

--- data.js
+++ data.js
@@ -1,7 +1,7 @@
 export const tweetsData = [
     {
         handle: `@TrollBot66756542 💎`,
-        profilePic: `/images/troll.jpg`,
+        profilePic: `images/troll.jpg`,
         likes: 27,
         retweets: 10,
         tweetText: `Buy Bitcoin, ETH Make 💰💰💰 low low prices. 
@@ -13,19 +13,19 @@ export const tweetsData = [
     },
     {
         handle: `@Elon ✅`,
-        profilePic: `/images/musk.png`,
+        profilePic: `images/musk.png`,
         likes: 6500,
         retweets: 234,
         tweetText: `I need volunteers for a one-way mission to Mars 🪐. No experience necessary🚀`,
         replies: [
             {
                 handle: `@TomCruise ✅`,
-                profilePic: `/images/tcruise.png`,
+                profilePic: `images/tcruise.png`,
                 tweetText: `Yes! Sign me up! 😎🛩`,
             },
             {
                 handle: `@ChuckNorris ✅`,
-                profilePic: `/images/chucknorris.jpeg`,
+                profilePic: `images/chucknorris.jpeg`,
                 tweetText: `I went last year😴`,
             },
         ],
@@ -35,7 +35,7 @@ export const tweetsData = [
     },
     {
         handle: `@NoobCoder12`,
-        profilePic: `/images/flower.png`,
+        profilePic: `images/flower.png`,
         likes: 10,
         retweets: 3,
         tweetText: `Are you a coder if you only know HTML?`,
